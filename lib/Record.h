6ff719041fd95aa07db10ffaaab610ebf5b41bcc
Joshua Lum <joshuanl@usc.edu>
2014-10-16 22:30:00 -0700

    finished record class

--- lib/Record.h
+++ lib/Record.h
@@ -1,12 +1,36 @@
 #ifndef RECORD_H
 #define RECORD_H 
 
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+struct MatchRecord{
+	int win;
+	int loss;
+	int fightCount;
+	string opponentName;
+	string opponentDeckName;
+};
+
 class Record
 {
 public:
-	Record(arguments);
+	Record(string deckName);
 	~Record();
+	void printRecord() const;
+	void printRecord(string opponentName) const;
+	void findHistoryAgainst(string opponent) const;
+	bool recordExists(string opponentName) const;
+	void updateRecord(string opponentName, int result);
+	void createNewRecord(string opponentName, string opponentDeckName, int result);
 
-	/* data */
+private:
+	string deckName;
+	int overallWins;
+	int overallLoss;
+	unordered_map<string, MatchRecord> history_map;
+	
 };
 #endif
--- src/Record.cpp
+++ src/Record.cpp
@@ -1,1 +1,63 @@
 #include "Record.h"
+#include <string>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+Record::Record(string deckName){
+	this->deckName = deckName;
+	overallWins = 0;
+	overAllLoss = 0;
+}//end of constructor
+
+Record::~Record(){
+	deckName = 0;
+	history_map.clear();
+}//end of destructor
+
+void Record::printRecord() const{
+	cout << endl << " >> Record:  [" << deckName << "]  -- " << overallWins << "-" << overAllLoss << endl;
+}//end of printing record
+
+void Record::printRecord(string opponentName) const{
+	iterator it = history_map.find(opponentName);
+	cout << " >> Record: [" << deckName << "] <<";
+	cout << (it->second).opponentName << " -- [" << (it->second).opponentDeckName << "]" << endl;
+	cout << "Fought: " << (it->second).fightCount  << endl;
+	cout << (it->second).win << " wins   " << (it->second).win << " losses " << endl;
+}//end of printing record;
+
+bool Record::recordExists(string opponentName) const{
+	iterator it = history_map.find(opponentName);
+	if (it == history_map.end()){
+		return false;
+	}
+	return true;
+}//end of exists func
+
+void Record::updateRecord(string opponentName, int result){
+	iterator it = history_map.find(opponentName);
+	(it->second).fightCount++;
+	if (result == 1){
+		(it->second).win++
+		return;
+	}
+	(it->second).loss++;
+}//end of update 
+
+void Record::createNewRecord(string opponentName, string opponentDeckName, int result){
+	MatchRecord temp;
+	temp.opponentName = opponentName;
+	temp.opponentDeckName = opponentDeckName;
+	temp.fightCount = 1;
+	if (result == 1){
+		temp.win = 1;
+		temp.loss = 0;
+	}
+	else{
+		temp.win = 0;
+		temp.loss = 1;
+	}
+	history_map.insert(opponentName, temp);
+}//end of creating a new record
